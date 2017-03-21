1a2edd17e811936611d0de2f361a09453fe97e52
Gregory Yoes-Favrot <gpyofavv@gmail.com>
2017-03-21 09:32:41 -0500

    Add Score, Needs Edit
    Added Score tracking and display. score shows up in editor, but not
    in-game.

---
+++ Assets/Scripts/ScoreTracker.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTracker : MonoBehaviour
+{
+    public TextMesh scoreText;
+
+    void Start()
+    {
+        scoreText = GameObject.Find("ScoreDisplay").GetComponent<TextMesh>();
+        scoreText.GetComponent<TextMesh>().text = "Score: " + TotalScore.score;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == "patron0")
+        {
+            TotalScore.score += 50;
+        }
+    }
+
+    void Update()
+    {
+        scoreText = GameObject.Find("ScoreDisplay").GetComponent<TextMesh>();
+        scoreText.GetComponent<TextMesh>().text = "Score: " + TotalScore.score;
+    }
+}
---
+++ Assets/Scripts/TotalScore.cs
@@ -0,0 +1,14 @@
+﻿/*
+ * Written by Jody Toms
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotalScore : MonoBehaviour
+{
+    public static int score = 0;
+
+    public static int ballCount = 2;
+}
