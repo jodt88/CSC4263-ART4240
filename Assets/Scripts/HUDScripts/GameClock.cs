d22232b3da511b9497a2a8cf69d93a24b06aa619
jodt88 <jtoms5@lsu.edu>
2017-04-13 14:18:41 -0500

    Add Game Clock
    Added a script that adds a clock that counts down from 10 minutes; needs
    a little tweaking, but works. Also added the clock and opponent's score
    to the HUD. Removed an old script that contained static variables to
    store the player and opponent's score, since Chris added a script that
    replaced it. Also added the "missing" scripts to their respective game
    objects.

---
+++ Assets/Scripts/HUDScripts/GameClock.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameClock : MonoBehaviour {
+
+	public static double hour = 10;
+	public static double minute = 00;
+	//public static double second = 00;
+
+	public static double lastChange = 0;
+
+	void Update () 
+	{
+		if (minute >= 0 && hour >= 0) 
+		{
+			if (Time.time - lastChange > 1.0) 
+			{
+				if (minute == 0) 
+				{
+					minute = 59;
+					hour--;
+				}
+				minute--;
+				lastChange = Time.time;
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/HUDScripts/ScoreTracker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTracker : MonoBehaviour
+{
+	public GUIStyle HUDStyle;
+
+	void OnGUI () 
+	{
+		GUI.Label (new Rect (130, 50, 100, 100), "Your Money: " + Inn.playerScore.ToString(), HUDStyle);
+		GUI.Label (new Rect (130, 80, 100, 100), "Rival Money: " + Inn.opponentScore.ToString(), HUDStyle);
+		GUI.Label (new Rect (130, 110, 100, 100), "Time Till Closing: " + GameClock.hour + ":" + GameClock.minute, HUDStyle);
+	}
+}
