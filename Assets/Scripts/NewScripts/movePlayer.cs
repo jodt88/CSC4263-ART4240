a510f25769e92647a5b0e965c87c6c52ce326b8d
jodt88 <jtoms5@lsu.edu>
2017-04-21 14:56:36 -0500

    Add move player script
    Added the missing script to allow the player to be moved by pressing Up,
    Down, Left or Right arrows, or WASD.

---
+++ Assets/Scripts/NewScripts/movePlayer.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movePlayer : MonoBehaviour
+
+{
+	void Update ()
+	{
+        // move the innkeeper left if player presses left-arrow or a
+		if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
+		{
+			Vector3 position = this.transform.position;
+			position.x -= (float).1;
+			this.transform.position = position;
+		}
+
+        // move the innkeeper right if player presses right-arrow or d
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+		{
+			Vector3 position = this.transform.position;
+			position.x += (float).1;
+			this.transform.position = position;
+		}
+
+        // move the innkeeper up if player presses up-arrow or w
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+		{
+			Vector3 position = this.transform.position;
+			position.y += (float).1;
+			this.transform.position = position;
+		}
+
+        // move the innkeeper down if player presses down-arrow or s
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
+		{
+			Vector3 position = this.transform.position;
+			position.y -= (float).1;
+			this.transform.position = position;
+		}
+	}
+}
