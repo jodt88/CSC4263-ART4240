de80b2771d0e1a7568399863a0845d4defed89fe
jodt88 <jtoms5@lsu.edu>
2017-04-23 18:12:00 -0500

    Add Title Screen and Cheat
    -Added a scene for the title screen.
    -Added a script that displays the title, and transitions to main scene
    (day 1). If the cheat code JOELSPEANUTS is typed in at the main screen,
    it will transition to main scene (day 7) with the inn's net profit set
    to 2 billion.

---
+++ Assets/Scripts/NewScripts/mainMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class mainMenu : MonoBehaviour {
+	public GUIStyle TitleStyle;	// settings for the GUI
+	public GUIStyle SubStyle;
+
+	// Variables for the cheat code
+	private int sequenceIndex;
+	private KeyCode[] sequence = new KeyCode[] {
+		KeyCode.J,
+		KeyCode.O,
+		KeyCode.E,
+		KeyCode.L,
+		KeyCode.S,
+		KeyCode.P,
+		KeyCode.E,
+		KeyCode.A,
+		KeyCode.N,
+		KeyCode.U,
+		KeyCode.T,
+		KeyCode.S
+	};
+
+	void OnGUI () 
+	{
+		// display the title screend
+		GUI.Label (new Rect (Screen.width/2-50, Screen.height/2-100, 100, 50), "InnKeeper", TitleStyle);
+		GUI.Label (new Rect (Screen.width/2-50, Screen.height/2+100, 100, 50), "Press the SPACEBAR to begin.", SubStyle);
+	}
+
+	void Update ()
+	{
+		// Start the game
+		if( Input.GetKeyDown(KeyCode.Space))
+		{
+			StartCoroutine (performFade3());
+		}
+
+		// Recognize the cheat JOELSPEANUTS
+		if (Input.GetKeyDown (sequence [sequenceIndex])) 
+		{
+			if (++sequenceIndex == sequence.Length) 
+			{
+				sequenceIndex = 0;
+				Inn.day = 6;						// Set day to 6, so when game starts its 7th day (last day)
+				Inn.playerScore_net = 2000000000;	// Set the inn's total profit to 2 billion (to guaruntee a win)
+				StartCoroutine (performFade3 ());
+			}
+		}
+	}
+
+	IEnumerator performFade3 ()
+	{
+		// fade out the scene
+		float fadeTime = GameObject.Find("ScreenFader").GetComponent<SceneFade>().BeginFade(1);    
+		yield return new WaitForSeconds(fadeTime);
+		SceneManager.LoadScene("main");
+	}
+}
